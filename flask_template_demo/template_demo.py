fb8009f0a6fb1fc81250d3310c1e63a0a2e35334
scrat <scratkong@163.com>
2019-05-24 16:33:31 +0800

    add self_define_template_filter

--- flask_template_demo/template_demo.py
+++ flask_template_demo/template_demo.py
@@ -17,6 +17,25 @@ def index():
                            my_array = my_array,
                            my_dict = my_dict
                            )
+# 自定义过滤器方式一： 添加列表反转的过滤器
+def do_listreverse(li):
+    # 通过原列表创建一个新列表
+    temp_li = list(li)
+    # 将新列表进行反转
+    temp_li.reverse()
+    return temp_li
+
+app.add_template_filter(do_listreverse,'lireverse')
+
+#自定义过滤器方式二：装饰器
+@app.template_filter('lireverse')
+def do_listreverser(li):
+    # 通过源列表创建一个新列表
+    temp_li = list(li)
+    # 将列表进行反转
+    temp_li.reverse()
+    return temp_li
+
 
 if __name__=="__main__":
     app.run()
